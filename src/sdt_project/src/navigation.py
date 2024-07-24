60a2dfab99c25c6704ff001ed7cf09842d0926d7
Yusuftaya1 <105513549+Yusuftaya1@users.noreply.github.com>
2024-07-24 15:29:36 +0300

    mode_status

--- src/sdt_project/src/navigation.py
+++ src/sdt_project/src/navigation.py
@@ -10,5 +10,12 @@ KONTROL EDİLİR
 
 "/cmd_vel" de KULLANILABİLİR
 
+  ---mode_status --- mode_status node--- 
+motor_values NEREDEN ALINACAK ONU BELİRLER
+
+1 == line_follow
+2 == turn
+3 == escape_block
+mode_status node
 """
 
