cdecab1b238b50873bb1097fa7e4c55d2fcb48cf
Yusuftaya1 <105513549+Yusuftaya1@users.noreply.github.com>
2024-08-02 21:08:41 +0300

    TCP_

--- src/sdt_project/src/TCP_com.py
+++ src/sdt_project/src/TCP_com.py
@@ -39,7 +39,7 @@ class TCP_Socket():
             self.client.close()
             self.connected = False
 
-    def send_sensor(self, send_to_ui):
+    def send_data(self, send_to_ui):
         msg_json = json.dumps(send_to_ui)
         self.data_transfer(msg_json.encode('utf-8'))
         print(f"Sent sensor data: {send_to_ui}")
@@ -50,18 +50,21 @@ class UI_sub(Node):
         self.socket = TCP_Socket()
         self.subscription = self.create_subscription(SensorValues, '/AGV/sensor_values', self.sensor_callback, 10)
         self.engel_status = self.create_subscription(String, 'engel_tespit', self.engel_callback, 10)
-        self.sensor_data = None
-        self.engel_statu = None
-        #self.timer = self.create_timer(1.0, self.merge_and_send)
-        
+        self.qr_status =    self.create_subscription(String,'/qr_code_data',self.qr_callback, 10)
+        self.sensor_data =  None
+        self.engel_statu =  None
+        self.qr_statu = None
+        self.timer = self.create_timer(1.0, self.merge_and_send)
+
     def sensor_callback(self, msg):
         self.sensor_data = msg
-        self.merge_and_send()
 
     def engel_callback(self, msg):
         self.engel_statu = msg
-        self.merge_and_send()
-        
+
+    def qr_callback(self,msg):
+        self.qr_statu = msg
+
     def merge_and_send(self):
         if self.sensor_data and self.engel_statu:
             msg_dict = {
@@ -73,9 +76,11 @@ class UI_sub(Node):
                 "lift_akim":          self.sensor_data.lift_akim,
                 "asiri_agirlik":      self.sensor_data.asiri_agirlik,
                 "engel":              self.engel_statu.data,
-                "map":                None
+                "map":                None,
+                "QR":                 self.qr_statu
             }
-            self.socket.send_sensor(msg_dict)
+            
+            self.socket.send_data(msg_dict)
 
 def main(args=None):
     rclpy.init(args=args)
